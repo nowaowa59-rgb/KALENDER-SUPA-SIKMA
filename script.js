5cb28ba1a3eacb626fa2208e7c16f12412990700
Naww_na <nowaowa59@gmail.com>
2026-09-08 13:48:27 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+echo "# KALENDER-SUPA-SIKMA" >> README.md
+git init
+git add index.html
+git add script.js
+git add style.css
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nowaowa59-rgb/KALENDER-SUPA-SIKMA.git
+git push -u origin main
